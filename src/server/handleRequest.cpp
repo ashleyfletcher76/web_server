98fd6334b8032c23a8c650b21958525d0d582737
Ashley Fletcher <asfletch@3-C-6.42heilbronn.de>
2024-08-08 15:28:01 +0200

    Last comment for updated version

--- src/server/handleRequest.cpp
+++ src/server/handleRequest.cpp
@@ -2,6 +2,7 @@
 
 void	HttpServer::handleGetRequest(int client_socket)
 {
+	// checks path from URI provided in request
 	std::string filePath = getFilePath(clientInfoMap[client_socket].request.uri);
 
 	std::ifstream file(filePath);
@@ -10,8 +11,12 @@ void	HttpServer::handleGetRequest(int client_socket)
 		sendErrorResponse(client_socket, 404, "Not Found");
 		return ;
 	}
-	std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	// read the whole content of the file
+	// first iterator points to beginning of the file
+	// second used an end marker, correct syntax
+	std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>()); // istreambuf_iterator is efficient for unformated data reading(raw bytes)
 	file.close();
+	// set response in the clients info
 	clientInfoMap[client_socket].response = formatHttpResponse(200, "OK", fileContent);
 }
 
@@ -26,6 +31,8 @@ void	HttpServer::handleRequest(int client_socket)
 		handleGetRequest(client_socket);
 	else
 		sendErrorResponse(client_socket, 501, "Not Implemented");
+
+	// set up write event for client response
 	struct kevent change;
 	EV_SET(&change, client_socket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
 	if (kevent(kq, &change, 1, NULL, 0, NULL) == -1)
