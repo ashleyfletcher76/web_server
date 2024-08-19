6f08afa6db3debacc72ac973db267c0b1556b908
Muhammet Kose <muhakose@3-C-9.42heilbronn.de>
2024-08-19 17:12:50 +0200

    small syntax

--- src/server/handleRequest/handleRequest.cpp
+++ src/server/handleRequest/handleRequest.cpp
@@ -17,21 +17,19 @@ bool	HttpServer::validateRouteAndMethod(int client_socket, const HttpRequest& re
 	const serverInfo &srv = serverIt->second->getServerInfo();
 	auto routeIt = srv.routes.find(request.uri);
 
-	if (routeIt == srv.routes.end())
+	if (routeIt != srv.routes.end())
 	{
-		//sendErrorResponse(client_socket, 404, "Not Found");
-		return (true);
-	}
-	const routeConfig &route = routeIt->second;
-	if (std::find(route.allowedMethods.begin(), route.allowedMethods.end(), request.method) == route.allowedMethods.end())
-	{
-		sendErrorResponse(client_socket, 405, "Method Not Allowed");
-		return (false);
-	}
-	if (!route.redirect.empty())
-	{
-		sendRedirectResponse(client_socket, route.redirect);
-		return (false);
+		const routeConfig &route = routeIt->second;
+		if (std::find(route.allowedMethods.begin(), route.allowedMethods.end(), request.method) == route.allowedMethods.end())
+		{
+			sendErrorResponse(client_socket, 405, "Method Not Allowed");
+			return (false);
+		}
+		if (!route.redirect.empty())
+		{
+			sendRedirectResponse(client_socket, route.redirect);
+			return (false);
+		}
 	}
 	return (true);
 }
@@ -83,80 +81,6 @@ void HttpServer::handleRequest(int client_socket)
 	processRequestMethod(client_socket);
 	registerWriteEvent(client_socket);
 }
-
-// // first iterator points to beginning of the file
-// // second used as end marker, correct syntax
-
-// void HttpServer::handleRequest(int client_socket)
-// {
-// 	HttpRequest &request = clientInfoMap[client_socket]->request;
-
-// 	auto serverIt = servers.find(clientInfoMap[client_socket]->server_fd);
-// 	if (serverIt == servers.end())
-// 	{
-// 		sendErrorResponse(client_socket, 500, "Internal Server Error");
-// 		return;
-// 	}
-// 	const serverInfo &srv = serverIt->second->getServerInfo();
-// 	std::string requestPath = request.uri;
-// 	auto routeIt = srv.routes.find(requestPath);
-
-// 	if (routeIt != srv.routes.end())
-// 	{
-// 		const routeConfig &route = routeIt->second;
-
-// 		if (std::find(route.allowedMethods.begin(), route.allowedMethods.end(), request.method) == route.allowedMethods.end())
-// 		{
-// 			sendErrorResponse(client_socket, 405, "Method Not Allowed");
-// 			return;
-// 		}
-// 		if (!route.redirect.empty())
-// 		{
-// 			sendRedirectResponse(client_socket, route.redirect);
-// 			return;
-// 		}
-// 	}
-
-// 	// decide to keep connection open based on HTTP response
-// 	if (request.headers.find("connection") != request.headers.end())
-// 	{
-// 		std::string connectionValue = request.headers["connection"];
-// 		trim(connectionValue);
-// 		std::transform(connectionValue.begin(), connectionValue.end(), connectionValue.begin(), ::tolower);
-// 		if (connectionValue == "keep-alive")
-// 			clientInfoMap[client_socket]->shouldclose = false;
-// 		else
-// 			clientInfoMap[client_socket]->shouldclose = true;
-// 	}
-// 	else
-// 		clientInfoMap[client_socket]->shouldclose = true;
-// 	if (request.method == "GET")
-// 		handleGetRequest(client_socket);
-// 	else if (request.method == "POST")
-// 		handlePostRequest(client_socket);
-// 	else
-// 		sendErrorResponse(client_socket, 501, "Not Implemented");
-
-// 	// set up write event for client response
-// 	struct kevent change;
-// 	EV_SET(&change, static_cast<uintptr_t>(client_socket), EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
-
-// 	// validate fd before using
-// 	if (fcntl(client_socket, F_GETFL) != -1)
-// 	{
-// 		if (kevent(kq, &change, 1, NULL, 0, NULL) == -1)
-// 			logger.logMethod("ERROR", "Kevent registration failure for writing: " + std::string(strerror(errno)));
-// 		else
-// 			logger.logMethod("INFO", "Succesfully registered kevent for socket: " + std::to_string(client_socket));
-// 	}
-// 	else
-// 	{
-// 		logger.logMethod("ERROR", "Attempted to register kevent for invalid FD: " + std::to_string(client_socket));
-// 		closeSocket(client_socket);
-// 	}
-// }
-// first iterator points to beginning of the file
-// second used as end marker, correct syntax
 
 void HttpServer::sendRedirectResponse(int client_socket, const std::string &redirectUrl)
 {
