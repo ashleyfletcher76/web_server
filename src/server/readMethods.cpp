742bd527e4ec47125b18e4835fd0e77ab2651de3
Ashley Fletcher <asfletch@1-B-13.42heilbronn.de>
2024-08-26 12:14:20 +0200

    Check my shh key

--- src/server/readMethods.cpp
+++ src/server/readMethods.cpp
@@ -41,7 +41,6 @@ void HttpServer::readRequest(int client_socket)
 		sendErrorResponse(client_socket, 404, "Error reading from socket");
 		return;
 	}
-
 	logger.logMethod("INFO", "Recieved request" + request);
 	if (request.empty() || !parseHttpRequest(request, clientInfoMap[client_socket].request, client_socket))
 	{
